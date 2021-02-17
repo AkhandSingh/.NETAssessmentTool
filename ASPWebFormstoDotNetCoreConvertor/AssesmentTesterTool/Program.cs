4b3db3291a2e11523ec51567361f1f89be5fb60c
Akhand Singh <akhand.bu05@gmail.com>
2021-02-17 11:03:23 +0530

    Initial Commit

---
+++ ASPWebFormstoDotNetCoreConvertor/AssesmentTesterTool/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Build.Evaluation;
+using ProjectAssementAPIBAL;
+using ProjectAssementAPIBAL.Interface;
+using System;
+
+namespace AssesmentTesterTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //if(args == null || args.Length == 0)
+            //{
+            //    Console.WriteLine("Enter Project Solution Path");
+            //}
+
+            var folder = @"D:\Dot Net\ToolConvertASPtoMVC\ToolConvertASPtoMVC.sln";
+
+            IProject project = new ProjectAssementAPIBAL.Implementation.Project();
+
+           var i = project.GetProjects(folder);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ASPWebFormstoDotNetCoreConvertor/ProjectAssessmentCommonEntities/ProjectModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ProjectAssessmentCommonEntities
+{
+    public class ProjectModel
+    {
+        public string ProjectName { get; set; }
+        public string ProjectType { get; set; }
+        public string ProjectPath { get; set; }
+    }
+}
