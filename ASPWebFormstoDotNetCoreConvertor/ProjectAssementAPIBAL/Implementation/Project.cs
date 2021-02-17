f31d29f83f73cff9476dd6a1951adf088cf6532a
AkhandSingh <akhand.bu05@gmail.com>
2021-02-17 10:56:27 +0530

    Add files via upload

---
+++ ASPWebFormstoDotNetCoreConvertor/ProjectAssementAPIBAL/Implementation/Project.cs
@@ -0,0 +1,32 @@
+﻿using ProjectAssementAPIBAL.Interface;
+using ProjectAssessmentCommonEntities;
+using System;
+using System.Collections.Generic;
+
+using Microsoft.Build.Construction;
+
+namespace ProjectAssementAPIBAL.Implementation
+{
+   public class Project: IProject
+    {
+        public List<ProjectModel> GetProjects(string solutonFile)
+        {
+            List<ProjectModel> projectModels = new List<ProjectModel>();
+            var solution = SolutionFile.Parse(solutonFile);
+            foreach(var project in solution.ProjectsInOrder)
+            {
+                var proj = new ProjectModel();
+                proj.ProjectName = project.ProjectName;
+                proj.ProjectType = project.ProjectType.ToString();
+                proj.ProjectPath = project.RelativePath;
+                projectModels.Add(proj);
+            }
+            return projectModels;
+        }
+
+        public List<dynamic> GetProjectDetails(string projectFile)
+        {
+            return new List<dynamic>();
+        }
+    }
+}
---
+++ ASPWebFormstoDotNetCoreConvertor/ProjectAssementAPIBAL/Interface/IProject.cs
@@ -0,0 +1,14 @@
+﻿using ProjectAssessmentCommonEntities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectAssementAPIBAL.Interface
+{
+    public interface IProject
+    {
+        List<ProjectModel> GetProjects(string solutonFile);
+
+        List<dynamic> GetProjectDetails(string projectFile);
+    }
+}
